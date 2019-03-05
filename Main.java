d82c602996f4ebfaf51d015820602be607b0c143
asbn223 <asbnjihoo@gmail.com>
2019-03-05 20:13:26 +0545

    First Commit

--- Main.java
+++ Main.java
@@ -1,12 +1,58 @@
-class Counter1
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+
+
+class Counter1 implements ActionListener
 {
-    Jframe frame;
-    Container content;
+    JFrame frame = new JFrame();
+    JLabel lblcount;
+    TextField tfCount;
+    Button btncount;
+    int count = 0;
 
     public static void main(String[] args) {
-        
+        Counter1 n = new Counter1();
+        n.frame1();
     }
-
+    void frame1()
+    {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
 
+    public Counter1() {
+        frame.setLayout(new FlowLayout());
+
+
+        lblcount = new JLabel("Counter");
+        frame.add(lblcount);
+
+        tfCount = new TextField(count + " "+ 10);
+        tfCount.setVisible(true);
+        frame.add(tfCount);
+
+        btncount = new Button("Count");
+        frame.add(btncount);
+        btncount.addActionListener(this);
+
+        frame.setSize(250,100);
+        frame.setTitle("Counter");
+        frame.setVisible(true);
+
+        
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        ++count;
+        tfCount.setText(count + "");
+
+    }
+    
 }
